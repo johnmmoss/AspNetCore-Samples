bdd4cc787c393d59f1061d06e023f0505c3c387d
John Moss <johnmmoss@gmail.com>
2022-11-19 17:24:50 +0000

    Add datables.net client side demo

---
+++ AspNetWebServerSidePagination/AspNetWebServerSidePagination/Pages/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Globalization;
+using CsvHelper;
+
+namespace AspNetWebServerSidePagination.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public IEnumerable<Country> Countries { get; set; }
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            var path = @"Data\countries.csv";
+            using (var reader = new StreamReader(path))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                Countries = csv.GetRecords<Country>().ToList();
+            }
+        }
+    }
+
+    public class Country
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Continent { get; set; }
+        public string Capital { get; set; }
+    }
+}
